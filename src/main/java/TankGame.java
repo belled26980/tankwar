d7afad15b79bc1fc09b0adc4046bf7bd224e12da
Andy <andy22179233@gmail.com>
2020-12-19 13:43:46 +0800

    視窗測試OK

--- src/main/java/TankGame.java
+++ src/main/java/TankGame.java
@@ -1,2 +1,13 @@
+import javax.swing.*;
+
 public class TankGame {
+    public static void main(String[] args) {
+        JFrame frame=new JFrame();
+        GameClient gameClient=new GameClient(1024,768);
+        frame.add(gameClient);
+        frame.setTitle("坦克大戰");
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+    }
 }
